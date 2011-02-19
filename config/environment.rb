89d6d677a55ff0e1d203de46531ee2f868b0c11f
Raul Souza Lima <raulsouzalima@gmail.com>
2011-02-19 03:44:33 -0200

    Removed some spaces on config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 GuruSpSite::Application.initialize!
 
-  Synthesis::AssetPackage.merge_environments = %w(staging production)
+Synthesis::AssetPackage.merge_environments = %w(staging production)

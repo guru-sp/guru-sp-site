797a3e3b9e6307476c746171c3c51b931c2967ae
Raul Souza Lima <raulsouzalima@gmail.com>
2011-02-19 04:53:42 -0200

    Add some files to ignore

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
-/* DO NOT MODIFY. This file was compiled Sat, 19 Feb 2011 06:28:57 GMT from
+/* DO NOT MODIFY. This file was compiled Sat, 19 Feb 2011 06:48:57 GMT from
  * /Users/raulsouzalima/Sites/guru-sp-site/app/coffeescripts/application.coffee
  */
 

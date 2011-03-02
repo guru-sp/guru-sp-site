c710024684718c9f63707deac7d59bf51388e139
marciogm <xhwoax@hotmail.com>
2011-03-02 13:27:12 -0300

    adicionaado metodo show no controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,15 @@
 class AlbumsController < ApplicationController
   
   def index
+    @albums = Album.albums
+  end
+
+  def show
+    @album = Album.photos(params[:id])
+
+    respond_to do |format|
+      format.html
+    end
   end
 
 end

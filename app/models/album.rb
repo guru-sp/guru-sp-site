51c2c6ef9d65399220eeed0c037f1fcfa63ec9f0
marciogm <xhwoax@hotmail.com>
2011-03-02 13:05:11 -0300

    adicionado self para os medodos

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,11 +4,11 @@ class Album
 
   PICASA_USER = "gurusp.org"
 
-  def albums
+  def self.albums
     Picasa.albums(:google_user => PICASA_USER)
   end
 
-  def photos(album_id)
+  def self.photos(album_id)
     photos = Picasa.photos(:google_user => PICASA_USER, :album_id => album_id)
     photos[:photos]
   end

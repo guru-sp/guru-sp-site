60ba5d5eb17c8e28dde0c7854e6820a14101642c
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-08-19 18:08:24 -0300

    habilitando asset pipeliner

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,8 @@ module GuruSpSite
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.assets.enabled = true
+
   end
 end

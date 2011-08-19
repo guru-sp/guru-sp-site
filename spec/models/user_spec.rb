dbb35eec5f6b0f9412871f6d0ec59ecf442635d2
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-08-19 18:55:54 -0300

    alterando testes de user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe User do
+  before(:all) do
+    Factory.create(:user, :email => "contato@guru-sp.org", :password => "algumasenhadificil", :github => "gurusp")
+  end
+  
+  subject do
+    Factory.build(:user, :email => "teste@guru-sp.org", :password => "algumasenhadificil", :github => "gurusp.org")
+  end
+  
   it { should validate_uniqueness_of :email }
   it { should validate_uniqueness_of :github }
-  
-  before(:each) do
-    Factory :user
-  end
 end

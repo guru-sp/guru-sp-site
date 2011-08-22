d64e315db2139f39a4b55a7e74de99e0bf00d02c
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-08-22 18:32:25 -0300

    removendo 'shoulds'

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -4,23 +4,23 @@ describe Album do
   
   context ".albums" do
     
-    it "should return all albums" do
+    it "returns all albums" do
       Album.albums.should be_an_instance_of(Array)
     end
     
-    it "album should contain a title" do
-      Album.albums.first.should have_key(:title)    
+    it "has a title" do
+      Album.albums.first.should have_key(:title)
     end
 
   end
 
   context ".photos" do
 
-    it "should return all photos" do
-      Album.photos(Album.albums.first[:id]).should be_an_instance_of(Array)      
+    it "returns all photos" do
+      Album.photos(Album.albums.first[:id]).should be_an_instance_of(Array)
     end
 
-    it "photos contain a thumbnail_1" do
+    it "has a thumbnail_1" do
       album_id = Album.albums.first[:id]
       photo = Album.photos(album_id)
       photo.first.should have_key(:thumbnail_1)

ede079114a13740d0642d803a0eeb920e3c20a47
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-09-01 11:46:09 -0300

    melhorando testes de aceitação para user

--- spec/acceptance/managing_member_spec.rb
+++ spec/acceptance/managing_member_spec.rb
@@ -7,31 +7,48 @@ feature "Managing Member", %q{
   I want to be able to login and appear as a member
 } do
 
+  let(:user) do
+    Factory :user, :password => "1234567890"
+  end
+
   scenario "sign up" do
     visit "/"
     click_link "Registrar-se"
-    fill_in 'Email', :with => "nathan@vieiraproenca.com"
-    fill_in 'user_password', :with => "secret"
-    fill_in 'user_password_confirmation', :with => "secret"
+    fill_in "Email", :with => "nathan@vieiraproenca.com"
+    fill_in "user_password", :with => "secret"
+    fill_in "user_password_confirmation", :with => "secret"
+    fill_in "user_github", :with => "secret"
     click_button "Registrar-se"
+    within ".flash" do
+      page.should have_content "Conectado com sucesso."
+    end
   end
-  
-  scenario "sign in" do
-    visit "/"
-    click_link 'Login'
-    fill_in 'Email' , :with => "nathan@vieiraproenca.com"
-    fill_in 'user_password', :with => "secret"
-    check "user_remember_me"
-    click_button "Entrar"
-    have_content "Sair"
+
+  scenario "successful sign in" do
+    login_with(user.email, "1234567890")
+    within ".flash" do
+      page.should have_content "Conectado com sucesso."
+    end
+    within "#user-navigation" do
+      page.should have_content "Perfil"
+      page.should have_content "Sair"
+    end
   end
-  
+
+  scenario "sign up with wrong password" do
+    login_with(user.email, "wrongpassword")
+    within ".flash" do
+      page.should have_content "Email ou senha inválidos."
+    end
+  end
+
   scenario "list users" do
     5.times do |number|
-      User.create :email => "user#{number}@wtv.com" , :password => "secret"
+      User.create :email => "user#{number}@wtv.com" , :github => "user_#{number}", :password => "secret"
     end
     visit "/users"
-    5.times {|n| have_content "user#{n}@wtv.com" }
+    5.times {|number| page.should have_content "user_#{number}" }
   end
 
 end
+
--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -1,5 +1,13 @@
 module HelperMethods
   # Put helper methods you need to be available in all tests here.
+  def login_with(email, password)
+    visit "/"
+    click_link "Login"
+    fill_in "Email" , :with => email
+    fill_in "user_password", :with => password
+    click_button "Entrar"
+  end
 end
 
 RSpec.configuration.include HelperMethods, :type => :acceptance
+

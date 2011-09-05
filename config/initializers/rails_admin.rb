35266d0f00c5dc04ccf88b23db52454ef6e74580
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-09-05 11:02:33 -0300

    adicionando ck_editor para editar descrição de encontro

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,5 +2,16 @@ RailsAdmin.config do |config|
   config.authorize_with do
     redirect_to "/" unless current_user.try(:admin?)
   end
+
+  config.model Meeting do
+    edit do
+      field :title
+      field :date
+      field :description, :text do
+        ckeditor true
+      end
+    end
+  end
+
 end
 

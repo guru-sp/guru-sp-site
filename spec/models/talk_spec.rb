3618d164b3dcf88083b96bb27491073bf45f6c45
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-09-08 17:32:18 -0300

    validações para talk e associação com meeting

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Talk do
   it { should validate_presence_of :title }
   it { should validate_presence_of :presenter_name }
-  it { should belong_to :meeting }
+  it { should belong_to :meeting  }
 end
 

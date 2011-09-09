606b61f3880ceda4b8e146ad8a952ccb1033e8fa
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2011-09-09 18:17:25 -0300

    obter próximo encontro

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,9 @@
 class Meeting < ActiveRecord::Base
   has_many :talks
   validates :title, :presence => true
+
+  def self.next_meeting
+    self.where("date >= ?", Date.today).order('date ASC').first
+  end
 end
 
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -3,5 +3,18 @@ require 'spec_helper'
 describe Meeting do
   it { should validate_presence_of :title }
   it { should have_many :talks }
+
+  describe ".next" do
+    before(:all) do
+      Factory(:meeting, :date => DateTime.now - 57.day)
+      Factory(:meeting, :date => DateTime.now + 60.day)
+      @next_meeting = Factory(:meeting, :date => DateTime.now + 27.day)
+    end
+
+    it "returns the nearest meeting from today" do
+      Meeting.next_meeting.should == @next_meeting
+    end
+  end
+
 end
 

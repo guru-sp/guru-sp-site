4e8912438c87143f59c470f1a76d6813a24022b0
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2012-06-04 15:11:57 -0300

    removing asset packager plugin

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GuruSpSite::Application.initialize!
-
-Synthesis::AssetPackage.merge_environments = %w(staging production)
-

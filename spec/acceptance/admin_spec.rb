877422d02d852c3a2c869b38135e64d8130d3aed
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2012-06-04 15:56:48 -0300

    correcting acceptance test

--- spec/acceptance/admin_spec.rb
+++ spec/acceptance/admin_spec.rb
@@ -7,18 +7,21 @@ feature "Accessing Admin Panel", %q{
   I want to be able to login and access the rails admin panel
 } do
 
+  let(:password) { "1234567890" }
+
   let(:admin_user) do
-    Factory :admin, :password => "1234567890"
+    Factory :admin, :password => password
   end
 
   let(:common_user) do
-    Factory :user, :password => "1234567890"
+    Factory :user, :password => password
   end
 
   scenario "successful sign in" do
     visit "/admin"
-    login_with(admin_user.email, "1234567890")
-    within ".flash" do
+    login_with(admin_user.email, password)
+    save_and_open_page
+    within ".alert" do
       page.should have_content "Conectado com sucesso."
     end
     page.should have_content "Administração do site"

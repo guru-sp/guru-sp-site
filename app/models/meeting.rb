4606925b7f9c9c02139acc0ea91219d199bf869a
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2012-06-25 21:36:51 -0300

    meeting should be ordered from the newest to the oldest

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,6 +2,8 @@ class Meeting < ActiveRecord::Base
   has_many :talks, :dependent => :destroy
   validates :title, :presence => true
 
+  default_scope order('date ASC')
+
   def self.next_meeting
     self.where("date >= ?", Date.today).order('date ASC').first
   end

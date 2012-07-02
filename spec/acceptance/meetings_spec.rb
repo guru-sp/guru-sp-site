17c9639d104613101cecbe20f6507f5a4f691a8e
Hugo Borges - agaelebe <hugo.borges@gmail.com>
2012-07-02 18:57:13 -0300

    renaming acceptance spec files

---
+++ spec/acceptance/meetings_spec.rb
@@ -0,0 +1,12 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+feature "Navigating Meetings", %q{
+  In order to know more about Guru-SP meetings
+  As a visitor of the site
+  I want to be know when is the next meeting, list the past meetings and see
+  details about
+} do
+
+
+end

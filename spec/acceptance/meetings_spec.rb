8a7bf888066ccfc708bf7786cdb563cfd6419b3e
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-07-24 20:59:01 -0300

    better description for this feature

--- spec/acceptance/meetings_spec.rb
+++ spec/acceptance/meetings_spec.rb
@@ -4,9 +4,8 @@ require 'spec_helper'
 feature "Navigating Meetings", %q{
   In order to know more about Guru-SP meetings
   As a visitor of the site
-  I want to be know when is the next meeting, list the past meetings and see
-  details about
+  I want to know when is the next meeting, list the past meetings and see
+  details about a given meeting
 } do
-
 
 end

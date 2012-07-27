9c385c8ab7c9b14a1f8d97aa4882413b558df80e
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-07-27 13:23:28 -0300

    validating venue name and adress

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,4 @@
 class Venue < ActiveRecord::Base
   attr_accessible :address, :details, :latitude, :longitude, :name
+  validates :name, :address, :presence => true
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Venue do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :address }
 end

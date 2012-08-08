459c556168619e096c4367263e857cde8ca9db2b
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-08-08 18:28:34 -0300

    just inverting

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,15 +1,15 @@
 class Album
+  
+  def self.all
+    Picasa.albums
+  end
 
   def initialize(album_id)
     @content = Picasa.photos(:album_id => album_id)
   end
 
   def photos
-    @content[:photos]
-  end
-
-  def self.all
-    Picasa.albums
+    @content[:photos] || []
   end
 
 end

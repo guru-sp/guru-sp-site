e8bfc1f75019d12181177b9f16316bb283fb2ce8
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-08-29 16:53:31 -0300

    updates all meetings to add slugs

--- db/migrate/20120829184658_add_slug_to_meetings.rb
+++ db/migrate/20120829184658_add_slug_to_meetings.rb
@@ -2,5 +2,8 @@ class AddSlugToMeetings < ActiveRecord::Migration
   def change
     add_column :meetings, :slug, :string
     add_index :meetings, :slug, :unique => true
+    
+    # updates all meetings to create a slug
+    Meeting.find_each(&:save)
   end
 end

7bf6a94ee8dec1c0d61dfe18a43af74420bfa21f
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-10-30 23:13:31 -0200

    add a simple acceptance test to meetings page (not complete yet but better than nothing)

--- spec/acceptance/meetings_spec.rb
+++ spec/acceptance/meetings_spec.rb
@@ -7,5 +7,25 @@ feature "Navigating Meetings", %q{
   I want to know when is the next meeting, list the past meetings and see
   details about a given meeting
 } do
+  
+  let(:meeting) do
+  end
+  
+  background "given that there is a visible meeting hapenning this year" do
+    FactoryGirl.create :meeting, :title => "Encontro 99",
+                       :date => Time.new(2012,11,10,9),
+                       :visible => true
+  end
+  
+  scenario "checking a meeting in meetings page" do
+    visit "/"
+    click_link "Encontros"
+    click_link "2012"
+    click_link "Encontro 99"
+    page.should have_content "Encontro 99"
+    within ".date h3" do
+      page.should have_content "Sábado, 10 de Novembro de 2012 às 09:00 hs"
+    end
+  end
 
 end

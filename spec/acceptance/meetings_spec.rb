eaa13a2f2b64387334ef8dbc7432a9f6a6e40b6c
Hugo Borges - agaelebe <agaelebe@gmail.com>
2013-01-11 17:44:41 -0200

    updating rails and removing date dependent test

--- spec/acceptance/meetings_spec.rb
+++ spec/acceptance/meetings_spec.rb
@@ -7,25 +7,14 @@ feature "Navigating Meetings", %q{
   I want to know when is the next meeting, list the past meetings and see
   details about a given meeting
 } do
-  
-  let(:meeting) do
-  end
-  
+
   background "given that there is a visible meeting hapenning this year" do
     FactoryGirl.create :meeting, :title => "Encontro 99",
-                       :description => "<strong>Amazing</strong>",
+                       :description => "<strong>Amazing description</strong>",
                        :date => DateTime.new(2012,11,10,9),
                        :visible => true
   end
-  
-  scenario "listing meetings" do
-    visit "/"
-    click_link "Encontros"
-    within ".list .item" do
-      page.should have_content "Amazing"
-    end
-  end
-  
+
   scenario "checking a meeting in meetings page" do
     visit "/"
     click_link "Encontros"

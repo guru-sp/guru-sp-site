4a0f447f604413125ce4f50226aec647d5ff723e
Hugo Borges - agaelebe <agaelebe@gmail.com>
2013-02-13 18:44:03 -0200

    configurint action_mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ GuruSpSite::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'gurusp.org' }
+
 end

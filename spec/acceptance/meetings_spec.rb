8e5f735fb19111981dde8e70d42e7f9e9b254331
Hugo Borges - agaelebe <agaelebe@gmail.com>
2013-09-04 16:48:18 -0300

    minor enhancements in tests and layout

--- spec/acceptance/meetings_spec.rb
+++ spec/acceptance/meetings_spec.rb
@@ -4,50 +4,41 @@ require 'spec_helper'
 feature "Navigating Meetings", %q{
   In order to know more about Guru-SP meetings
   As a visitor of the site
-  I want to know when is the next meeting, list the past meetings and see
-  details about a given meeting
+  I want to know when is the next meeting, list the past meetings and
+  see details about a given meeting
 } do
+
+  let(:title) { "Encontro 99" }
 
   background "given that there is a visible meeting hapenning this year" do
     
     caelum = FactoryGirl.create :sponsor, :name => "Caelum",
                                 :url => "http://caelum.com.br"
 
-    FactoryGirl.create :meeting, :title => "Encontro 99",
+    FactoryGirl.create :meeting, :title => title,
                        :description => "<strong>Amazing description</strong>",
                        :date => DateTime.new(2012,11,10,9),
                        :visible => true,
                        :sponsors => [caelum]
   end
 
-  def navigate_to_meeting_99
+  def navigate_to_meeting
     visit "/"
     click_link "Encontros"
     click_link "2012"
-    click_link "Encontro 99"
+    click_link title
   end
 
   scenario "checking a meeting date in meetings page" do
-    navigate_to_meeting_99
+    navigate_to_meeting
     within ".date h3" do
       page.should have_content "Sábado, 10 de Novembro de 2012 às 09:00 hs"
     end
   end
 
   scenario "checking a meeting title, sponsor inside meeting's page" do
-    navigate_to_meeting_99
-    page.should have_content "Encontro 99"
-    within ".sponsors" do
-      page.should have_link "Caelum"
-    end
-  end
-
-  scenario "checking a meeting title, sponsor inside meeting's page" do
-    visit "/"
-    click_link "Encontros"
-    click_link "2012"
-    click_link "Encontro 99"
-    page.should have_content "Encontro 99"
+    navigate_to_meeting
+    page.should have_content title
     within ".sponsors" do
       page.should have_link "Caelum"
     end
